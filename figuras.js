58ce5bdd0ffeac92f1d5ca05f653c5b336dedadb
juankrlos861122 <juancho_861122@hotmail.com>
2022-05-30 00:17:30 -0500

    Hola

---
+++ figuras.js
@@ -0,0 +1,8 @@
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cms");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado  + " cms");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + " cms");
